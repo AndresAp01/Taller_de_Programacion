a354d7dd49bc89ca8b74964708a1b3fdeed6c318
Luis Andrés Acuña Pérez <70827561+AndresAp01@users.noreply.github.com>
2025-02-25 09:55:36 -0600

    Added file via upload

---
+++ Semana 02/practica03funciones.py
@@ -0,0 +1,23 @@
+#Funciones
+"""def Nombre():
+        acciones"""
+
+#ejemplo sin parametros:
+def saludo():
+    print("Hola a todos")
+
+def EjemploCondicionales(x):
+#probar con 24, 25, 26, 27, 27.5     
+    if (x%4)==0:
+        print(x*x)
+    elif (x%4)==1:
+        print(x/6)
+    elif (x%4)==2:
+        print(((x**2)*(x**3))**2)
+    elif (x%4)==3:
+        print(x**3+5)
+    else:
+        print("No hay nada que hacer")
+
+
+
