2a725ef34b1f04ffbcf9b0b9814730f10cc7ceb4
Luis Andrés Acuña Pérez <andres.acunaperez@protonmail.com>
2025-03-10 23:49:05 -0600

    updated readme.md and updated files

--- Tareas/tarea_5_Luis_Acunna_Perez_taller.py
+++ Tareas/tarea_5_Luis_Acunna_Perez_taller.py
@@ -4,21 +4,26 @@
 def funcion1(lista):
     if type(lista)==list:
         if lista==[]:
-            return 0
-        else:
-            suma=0
-            multi=1
-            ultimo=lista[0]
-            while lista!=[]:
-                
-                if lista[0]%2==0:
-                    multi=multi*(ultimo**3)
-                
-                else:
-                    suma=suma+(ultimo**2)
-                
-                lista=lista[1:]
-            print("la suma de los impares es: ", suma, "la multiplicacion de los pares: ", multi)
+            return 0, 1
 
+        suma = 0
+        multi = 1
+
+        lista_copia = lista[:]
+        while lista_copia:
+            ultimo = lista_copia[0]
+            if ultimo%2==0:
+                multi = multi*(ultimo**3)
+            else:
+                suma = suma+(ultimo**2)
+
+            lista_copia = lista_copia[1:]
+
+        print("La suma de los impares es: ",suma, "y la multipllicacion de lois pares es:", multi)
+        return suma, multi
+
+    else:
+        print("Debe escribir una lista")
 #Suma todos los valores impares y ^2
 #Multiplica todos los valores impares y ^3
+    
