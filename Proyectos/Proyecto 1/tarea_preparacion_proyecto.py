53819ee36d9838b7ef808b351c020a003ff3a743
Luis Andrés Acuña Pérez <andres.acunaperez@protonmail.com>
2025-03-16 02:48:11 -0600

    added code

--- Proyectos/Proyecto 1/tarea_preparacion_proyecto.py
+++ Proyectos/Proyecto 1/tarea_preparacion_proyecto.py
@@ -17,25 +17,47 @@ def mostrar_menu(opciones):
 
 def MainMenu():
     opciones = ["Insercion", "Buscar", "Salir"]
-    subopciones = [] #Para poder ingresar a otro ciclo y muestre un segundo menu
+    subopciones = ["Pais", "Ciudad", "Restaurante", "Menu", "Productos", "Clientes", "Regresar al mantenimiento"] #Para poder ingresar a otro ciclo y muestre un segundo menu
 
     while True:
-        mostrar_menu(subopciones)
-
-
+        mostrar_menu(opciones)
+        print("\n Ingrese que quiere hacer: ")
         x = int(input())
         if x == 1:
             pass
         elif x == 2:
+            print("Has seleccionado Buscar.")
             while True:
-                print("1 para verificar Pais")
-                print("2 para buscar Ciudad")
-                print("3 para buscar restaurante")
-                print("4 para buscar menu")
-                print("5 para buscar productos")
-                print("6 para buscar cliente")
-                break
-            continue
+                # Mostrar submenú
+                mostrar_menu(subopciones, "\n Puede buscar por: ")
+                y = int(input("Selecciona una subopción (1-7): "))
+                if y == int():
+                    pass
+                else:
+                    print("Por favor, ingresa un número válido.")
+                    continue
+                if y == 1:
+                    print("Has seleccionado buscar Pais.")
+                elif y == 2:
+                    print("Has seleccionado buscar Ciudad.")
+                elif y == 3:
+                    print("Has seleccionado buscar Restaurante.")
+                elif y == 4:
+                    print("Has seleccionado buscar Menu.")
+                elif y == 5:
+                    print("Has seleccionado buscar Productos.")
+                elif y == 6:
+                    print("Has seleccionado buscar Clientes.")
+                elif y == 7:
+                    print("Volviendo al menú principal...")
+                    break  # Salir del submenú y volver al menú principal
+                else:
+                    print("Opción no válida. Por favor, selecciona una subopción del 1 al 7.")
         elif x == 3:
             break
+        else:
+            print("Ingresa una opcion correcta")
+            continue #Para que el usuario no tenga que reiniciar el programa
+
 MainMenu()
+
