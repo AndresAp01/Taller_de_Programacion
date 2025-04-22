702c471f0706e4e5d3497b380022097f00816804
Luis Andrés Acuña Pérez <70827561+AndresAp01@users.noreply.github.com>
2025-04-22 22:00:06 +0000

    Update

--- sem9_intro.py
+++ sem9_intro.py
@@ -136,7 +136,27 @@ def PromedioRPD_aux(lista):
 resultado=Operacion([6,1,2,3,5,7,8])
 print(resultado)"""
 
-def operarDes(lista):
+def WhileSD(lista):
+    if type(lista)!=list or len(lista)==0:
+        return "Error"
+    nueva_lista=[]
+    largo=len(lista)
+    i=0
+    while i<largo:
+        base=lista[i]
+        suma=lista[(i+2)%largo]
+        resta=lista[(i+4)%largo]
+        resultado=base+suma-resta
+        nueva_lista.append(resultado)
+        i+=1
+    return nueva_lista
+print(WhileSD([6,1,2,3,5,7,8]))
+    
+'''
+        if lista==[]:
+            return 0
+        else: 
+            return WhileDes_aux(lista,0,0)
     if len(lista)<3:
         return"Error"
     resultado=[0]*len(lista)
@@ -148,7 +168,25 @@ def operarDes(lista):
 
         resultado[i]=lista[0]+lista[2:3]-lista[1:5]
         i+=1
-    return resultado
+    return resultado'''
+
+def WhileD(lista):
+    if type(lista)!=list or len(lista)==0:
+        return "Error"
+    nueva_lista=[]
+    largo=len(lista)
+    i=0
+    copia=lista
+    while lista!=[]:
+        base=lista[0]
+        suma=copia[(i+2)%largo]
+        resta=copia[(i+4)%largo]
+        resultado=base+suma-resta
+        nueva_lista.append(resultado)
+        lista=lista[1:]
+        i+=1
+    return nueva_lista
+print(WhileD([6,1,2,3,5,7,8]))
 
 def OperarSD(lista):
     if len(lista)<3:
@@ -172,3 +210,30 @@ def operar(lista):
 res=operar([1,2,3,4])
 print(res)
 
+def Operar_For_Range(lista):
+    if type(lista)!=list or len(lista)==0:
+        return"Error"
+    resultado=[0]*len(lista)
+    for i in range(len(lista)):
+        indice_0=i%len(lista)
+        indice_2=(i+2)%len(lista)
+        indice_4=(i+4)%len(lista)
+
+        resultado[i]=lista[indice_0]+lista[indice_2]-lista[indice_4]
+    return resultado
+print(Operar_For_Range([6,1,2,3,5,7,8]))
+
+def Operar_For_i(lista):
+    if type(lista)!=list or len(lista)==0:
+        return"Error"
+    resultado=[0]*len(lista)
+    i=0
+    for x in lista:
+        indice_0=i%len(lista)
+        indice_2=(i+2)%len(lista)
+        indice_4=(i+4)%len(lista)
+
+        resultado[i]=lista[indice_0]+lista[indice_2]-lista[indice_4]
+        i+=1
+    return resultado
+print(Operar_For_i([6,1,2,3,5,7,8]))
