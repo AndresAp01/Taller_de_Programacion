18616ecb3d23edd0276bed8cf84bd87aa9f4f830
Luis Andrés Acuña Pérez <andres.acunaperez@protonmail.com>
2025-05-04 00:11:25 -0600

    update

--- Examenes_Intro/2_Luis_Acunna_Perez.py
+++ Examenes_Intro/2_Luis_Acunna_Perez.py
@@ -3,6 +3,19 @@
 
 #funciones auxiliares copiadas y pegadas del archivo de la profe:
 #validacion de matriz
+
+def valida_matriz(matriz):
+    if not isinstance(matriz, list) or len(matriz) == 0:
+        return False
+    primera = matriz[0]
+    if not isinstance(primera, list) or len(primera) == 0:
+        return False
+    cols = len(primera)
+    for fila in matriz:
+        if not isinstance(fila, list) or len(fila) != cols:
+            return False
+    return True
+
 def ValidaMatriz(matriz):
     a = 0
     b = 0
@@ -92,6 +105,9 @@ def UNO(lista):
                     return [lista]
         else:
             return "La lista no es un conjunto, por favor remover digitos repetidos."
+    else:
+        return 0
+
 
 #resultado=UNO([6, -1, 2, 7, 8, 16, 100, -200])
 #print(result)
@@ -122,11 +138,99 @@ def DOS(num1, num2):
                 digitos2inverso=(num2//(10**i))%10
                 if i%2==0:
                     nuevo_num1=nuevo_num1*10+digitos1
-                    nuevo_num2=nuevo_num2*10+digitos2
+                    nuevo_num2=nuevo_num2*10+digitos2inverso
+                else:
+                    nuevo_num1=nuevo_num1*10+digitos2
+                    nuevo_num2=nuevo_num2*10+digitos1inverso
+            print(nuevo_num1, nuevo_num2)
+            nuevo_largo=largofor(nuevo_num1)
+            if nuevo_largo%5==0 and nuevo_largo%3==0:
+                lista1=[]
+                lista2=[]
+                grupos=nuevo_largo//3
+                for j in range(grupos):
+                    vamos=nuevo_largo-(j+1)*3
+                    bloque1=(nuevo_num1//(10**vamos))%1000
+                    bloque2=(nuevo_num2//(10**vamos))%1000
+                    separar=bloque1//100
+                    separar11=(bloque1//10)%10
+                    separar12=bloque1%10
+
+                    separar2=bloque2//100
+                    separar21=(bloque2//10)%10
+                    separar22=bloque2%10
+
+                    sub1=[separar**2, separar11**3, separar12**2]
+                    sub2=[separar2**2, separar21**3, separar22**2]
+                    lista1=lista1+[sub1]
+                    lista2=lista2+[sub2]
+                if not (valida_matriz(lista1) and valida_matriz(lista2)):
+                    return "Matrices inválidas"
+
+                    # 2) Sumarlas elemento a elemento
+                suma = []
+                for i in range(len(lista1)):
+                    fila_suma = []
+                    for j in range(len(lista1[i])):
+                        fila_suma = fila_suma + [lista1[i][j] + lista2[i][j]]
+                    suma = suma + [fila_suma]
+
+                return suma
+            elif largofor(nuevo_num1)!=largofor(nuevo_num2):
+                return "Los nuevos numeros quedaron de diferentes largos, no se puede construir la lista"
+            elif nuevo_largo%5==0 and nuevo_largo%3!=0:
+                lista1=[]
+                lista2=[]
+                center=nuevo_largo//2
+                shift1=nuevo_largo-3
+                bloque1=(nuevo_num1//(10**shift1))%1000
+                bloque2=(nuevo_num2//(10**shift1))%1000
+                d10=bloque1//100
+                d11=(bloque1//10)%10
+                d12=bloque1%10
+
+                d20=bloque2//100
+                d21=(bloque2//10)%10
+                d22=bloque2%10
+
+                lista1=lista1+[[d10**2, d11**3, d12**2]]
+                lista2=lista2+[[d20**2, d21**3, d22**2]]
+                # segunda sublista
+                shift2=nuevo_largo-(center+3)
+                bloque1=(nuevo_num1//(10**shift2))%1000
+                bloque2=(nuevo_num2//(10**shift2))%1000
+
+                d10=bloque1//100
+                d11=(bloque1//10)%10
+                d12=bloque1%10
+
+                d20=bloque2//100
+                d21=(bloque2//10)%10
+                d22=bloque2%10
+
+                lista1=lista1+[[d10**2, d11**3, d12**2]]
+                lista2=lista2+[[d20**2, d21**3, d22**2]]
+
+                if not (valida_matriz(lista1) and valida_matriz(lista2)):
+                    return "Matrices invalidas"
+                suma=[]
+                for i in range(len(lista1)):
+                    fila_suma=[]
+                    for j in range(len(lista1[i])):
+                        fila_suma=fila_suma+[lista1[i][j] + lista2[i][j]]
+                    suma=suma+[fila_suma]
+
+                return suma
+            else:
+                return "Algo no previsto ha pasao"
     else:
         return "Los numeros no son enteros"
 
-print(DOS(1234,1234))
+print(DOS(61257,89102))
+#print(DOS(133557789214365,745230987664422))
+#NUEVOS NUM = 143250789614325 143250789614325
+#LISTAS = ([[1, 4, 3], [2, 5, 0], [7, 8, 9], [6, 1, 4], [3, 2, 5]], [[1, 4, 3], [2, 5, 0], [7, 8, 9], [6, 1, 4], [3, 2, 5]])
+
 
 def TRES():
     pass
